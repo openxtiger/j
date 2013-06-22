d6ed873c3c9d2c535116265a5acaa1be93f85f97
xtiger <openxtiger@sina.com>
2013-06-22 23:02:18 +0800

    first commit

---
+++ api/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Jclass",
+        "jui.Component",
+        "junit.BaseTemplate",
+        "junit.JTemplate",
+        "junit.MixedCollection",
+        "junit.Observable",
+        "junit.Observer",
+        "junit.STemplate",
+        "junit.Template",
+        "oop"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
