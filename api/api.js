495009154a0eacad0acd7cb0ea1f897c5e8831ba
xtiger <openxtiger@sina.com>
2014-02-23 20:57:04 +0800

    changes

--- api/api.js
+++ api/api.js
@@ -1,14 +1,31 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "JDEngine",
         "Jclass",
         "Opp",
+        "dataview",
+        "jde.DataProxy",
+        "jde.JsonReader",
+        "jdengine",
+        "jsonreader",
         "jui.Component",
+        "jui.DataView",
+        "jui.Layer",
+        "jui.form.Checkbox",
+        "jui.form.ComboBox",
+        "jui.form.Field",
+        "jui.form.Hidden",
+        "jui.form.NumberFiled",
+        "jui.form.Radio",
+        "jui.form.TextArea",
+        "jui.form.TextField",
+        "jui.form.TriggerField",
+        "jui.form.TwinTriggerField",
         "junit.BaseTemplate",
         "junit.JTemplate",
         "junit.MixedCollection",
         "junit.Observable",
-        "junit.Observer",
         "junit.STemplate",
         "junit.Template"
     ],
@@ -35,7 +52,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "OOP",
             "name": "OOP",
-            "description": "The global namespace methods,Used to simulate the Java keyword.<br/>\n模拟Java的面向对象功能，在此次分别模拟了 package,class,extends,public,private,protected,static,import\n可以用以下形式定义类\n\n    'class C extends P implements I'.j(function(jsuper){\n\n          jstatic({\n              a:1\n          });\n\n          jpublic({\n              constructor: function () {\n                  this.jsuper();\n              },\n              echo:function(){\n                  console.log(this.jstatic.a);\n              }\n          });\n\n          jprotected({\n\n          });\n\n          jprivate({\n\n          });\n     },'alias class name');"
+            "description": "The global namespace methods,Used to simulate the Java keyword.<br/>\n模拟Java的面向对象功能，在此次分别模拟了 package,class,extends,public,private,protected,static,import\n可以用以下形式定义类\n\n    'class C extends P implements I'.j(function(csuper){\n\n          jstatic({\n              a:1\n          });\n\n          jpublic({\n              constructor: function () {\n                  jsuper(this);\n              },\n              echo:function(){\n                  console.log(jstatic().a);\n              }\n          });\n\n          jprotected({\n\n          });\n\n          jprivate({\n\n          });\n     },'alias class name');"
         }
     ]
 } };
